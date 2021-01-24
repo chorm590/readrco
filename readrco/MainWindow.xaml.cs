02904d544bec3f6d54e61e85f72bae55c5b3c531
chorm <120085336@qq.com>
2021-01-24 22:08:47 +0800

    deving...

---
+++ readrco/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using readrco.src.tool;
+
+namespace readrco
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		private const string TAG = "readrco";
+
+		private NewRecord newRecordWindow;
+
+		public MainWindow()
+		{
+			InitializeComponent();
+			Logger.v(TAG, "hello world");
+
+		}
+
+		private void Btn_New_Click(object sender, RoutedEventArgs e)
+		{
+			Logger.v(TAG, "Newing book-read-record");
+			if(newRecordWindow != null)
+			{
+				newRecordWindow.Close();
+			}
+
+			newRecordWindow = new NewRecord();
+			newRecordWindow.Show();
+		}
+	}
+}
---
+++ readrco/NewRecord.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+using readrco.src.tool;
+
+namespace readrco
+{
+	/// <summary>
+	/// Interaction logic for NewRecord.xaml
+	/// </summary>
+	public partial class NewRecord : Window
+	{
+		private const string TAG = "NewRecord";
+
+
+		public NewRecord()
+		{
+			InitializeComponent();
+
+			GBStar.Visibility = Visibility.Collapsed;
+			
+		}
+
+		private void Read_Status_Watcher(object sender, RoutedEventArgs e)
+		{
+			Logger.v(TAG, "Read_Status_Watcher()");
+		}
+
+		private void Add_Author(object sender, RoutedEventArgs e)
+		{
+			Logger.v(TAG, "Add_Author()");
+			TextBox tbox = new TextBox();
+			tbox.Width = 200;
+
+			SPAuthors.Children.Add(tbox);
+		}
+	}
+}
